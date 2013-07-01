7b632beca26982799b8bb95bace521c8b57e1a86
Jay Chetty <jaypaulchetty@gmail.com>
2013-07-01 12:44:48 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeddingApp::Application.config.secret_token = '1a167be16a5b7aacf06560ebac53f737921bf022624b6da2e5531a3eaef7775565e3996866ddd39f958c2f35c4686f1620804fbafd10d826828bf138636eddd7'
